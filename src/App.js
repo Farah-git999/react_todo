bb1f9d708cdf158db5ec4c261d3050b16df40384
Farah-git999 <56755167+Farah-git999@users.noreply.github.com>
2020-06-12 02:15:08 +0500

    update message again

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Learning React in Bootcamp 2020</h1>
+      <h1>Learning React in Bootcamp</h1>
     </div>
   );
 }
