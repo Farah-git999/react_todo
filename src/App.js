b3adc0ad170864bad9f706c7958abd1c536a8ad5
Farah-git999 <56755167+Farah-git999@users.noreply.github.com>
2020-06-12 02:03:42 +0500

    Remove logo items

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learning React in Bootcamp!
-        </a>
-      </header>
+      <h1>Learning React in Bootcamp 2020</h1>
     </div>
   );
 }
