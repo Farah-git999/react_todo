c370e6dbe4e2da10813a37b11f111fd85024de80
Farah-git999 <56755167+Farah-git999@users.noreply.github.com>
2020-06-13 00:55:16 +0500

    Create Student.js

---
+++ src/Student.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Student (props) {
+  return(
+    <div>
+      <h1>Name of the student is {props.name}</h1>
+      <h1>Class of the student is {props.class}</h1>
+    </div>
+  )
+}
+
+export default Student;
